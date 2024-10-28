ef5c839a589819fa0a13cecafb0906e4be15614c
Huzefa <huzefaici@gmail.com>
2024-10-28 10:37:05 +0500

    authentication

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,25 @@ const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const errorHandler = require('./middleware/error')
 
 require('dotenv').config()
 var cors = require('cors')
+const cookieParser = require('cookie-parser')
 const app = express();
 
 //DB
 mongoose.connect(process.env.DATABASE).then(()=>console.log('Db connected')).catch((err)=>  console.log(err))
+
+//middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json({limit: '5mb'}))
+app.use(bodyParser.urlencoded({
+    limit: '5mb',
+    extended: true
+}))
+app.use(cookieParser())
+app.use(cors())
 
 const port = process.env.PORT || 8000
 
---
+++ backend/utils/errorResponse.js
@@ -0,0 +1,8 @@
+class errorResponse extends Error {
+    constructor(message, codeStatus) {
+        super(message),
+        this.codeStatus= codeStatus    
+    }
+}
+
+module.exports = errorResponse
